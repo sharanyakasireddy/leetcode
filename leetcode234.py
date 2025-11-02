f52ae6b7c12d71154659c16b9d7f17ad898e8d2f
sharanyakasireddy <sharanyakasireddy18@gmail.com>
2025-11-03 00:45:02 +0530

    palindrome linkedlist

--- leetcode234.py
+++ leetcode234.py
@@ -12,3 +12,28 @@ class Solution:
 
 optimal method-
 
+class Solution:
+    def isPalindrome(self, head):
+        # Finding the middle of the list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        #Reverse the second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        #Compare both halves
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
